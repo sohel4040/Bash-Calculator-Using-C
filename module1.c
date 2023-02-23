dc5bd1040dc3bd39f9fc97367f117b36632b7b27
Sohel <sohelbargir2@gmail.com>
2023-02-23 16:02:25 +0530

    Stack implementation done for custom numbers

---
+++ module1.c
@@ -0,0 +1,111 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"linkedlist.c"
+
+typedef struct element
+{
+    // int data;
+    List *number;
+    struct element * next;    
+} element;
+
+typedef struct stack
+{
+    element* top;
+} stack;
+
+void initstack(stack *s)
+{
+    s->top = NULL;
+}
+
+int isEmpty(stack s)
+{
+    return (s.top == NULL);
+}
+
+void push(stack *s,List *num)
+{
+    element* newnode = (element*) malloc(sizeof(element));
+
+    if(!newnode)
+    return;
+
+    newnode->number=num;
+    newnode->next = NULL;
+
+    if(isEmpty(*s))
+    {
+        s->top = newnode;
+        return;
+    }
+
+    newnode -> next = s -> top;
+    s -> top = newnode;
+
+}
+
+List* pop(stack *s)
+{
+    if(isEmpty(*s))
+    return NULL;
+
+    List *num = s->top->number;
+    element * temp = s-> top;
+    s->top = s->top->next;
+    free(temp);
+
+    return num;
+
+}
+
+List* top(stack s)
+{   
+    return s.top->number;
+}
+
+// void display(stack *s)
+// {
+//     element *temp = s->top;
+
+//     while(temp)
+//     {
+//         printf("%d\t",temp->data);
+//         temp = temp -> next;
+//     }
+//     printf("\n");
+// }
+int main()
+{
+    stack s;
+    initstack(&s);
+    List l1, l2;
+    initList(&l1);
+    initList(&l2);
+    
+    insertToFront(&l1, 9);
+    insertToFront(&l1, 5);
+
+    insertToFront(&l2, 2);
+    insertToFront(&l2, 1);
+
+    displayReverse(l1);
+    printf("\n");
+    displayReverse(l2);
+    printf("\n");
+
+    
+    push(&s, &l1);
+    push(&s, &l2);
+
+    pop(&s);
+    List *l = top(s);
+
+
+    displayReverse(*l);
+
+
+
+    return 0;
+    
+}
