c2b9d2cb8a7b93be08fa9be8b17bb7046e40aaa1
Sohel Bargir <110667879+sohel4040@users.noreply.github.com>
2023-03-20 15:25:20 +0530

    Final Version

--- linkedlist.h
+++ linkedlist.h
@@ -9,9 +9,11 @@ typedef node* List;
 void initList(List *l);
 void insertToFront(List *l, int d);
 void append(List *l, int d);
+int deleteFirst(List *l);
 int length(List l);
 int isZero(List l);
 int compare(List l1, List l2);
+void reverse(List *l1);
 List addTwoLinkedLists(List *l1, List *l2);
 List substractTwoLinkedLists(List *l1, List *l2);
 List multiplyTwoLinkedLists(List *l1, List *l2);
@@ -22,4 +24,9 @@ List power(List *l1, List *l2);
 void removeRedundentZeros(List *l);
 void displayList(List l);   
 void displayReverse(node *l);
+
+void truncateDecimalPointDigits(List *num, int afterDecimal, int needToTruncate);
+List trimLinkedList(List l1, int count);
+void displayNumber(List num, int count);
+int checkIfNumberIsTendsToZero(List l, int count);
 
